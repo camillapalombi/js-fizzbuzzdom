46ceef183eceff90ea2cb6d44a318e3b7ebd5692
camillapalombi <camilla.palombi14@gmail.com>
2022-02-23 17:39:35 +0100

    Aggiunti fizz/buzz e fizzbuzz

--- js/script.js
+++ js/script.js
@@ -2,9 +2,23 @@ let squaresContainer = document.querySelector('.squares-container');
 
 
 for (let i = 1; i <= 100; i++) {
+
+    if (i % 15 == 0) {
+
+        squaresContainer.innerHTML = squaresContainer.innerHTML + `<div class="square red"> FizzBuzz </div>`;
+
+    } else if (i % 3 == 0) {
     
-    console.log(i)
+        squaresContainer.innerHTML = squaresContainer.innerHTML + `<div class="square green"> Fizz </div>`;
     
-    squaresContainer.innerHTML = squaresContainer.innerHTML + `<div class="square">${i}</div>`;
+    } else if (i % 5 == 0) {
+    
+        squaresContainer.innerHTML = squaresContainer.innerHTML + `<div class="square yellow"> Buzz </div>`;
+    
+    } 
+    else {
+        squaresContainer.innerHTML = squaresContainer.innerHTML + `<div class="square">${i}</div>`;
+    }
     
 }
+
