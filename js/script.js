3e4078f67f65bb5e0c612595aaee2014cd553b67
camillapalombi <camilla.palombi14@gmail.com>
2022-02-23 19:10:20 +0100

    Attuato metodo 'APPEND'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let squaresContainer = document.querySelector('.squares-container');
+/*let squaresContainer = document.querySelector('.squares-container');
 
 
 for (let i = 1; i <= 100; i++) {
@@ -14,11 +14,49 @@ for (let i = 1; i <= 100; i++) {
     } else if (i % 5 == 0) {
     
         squaresContainer.innerHTML = squaresContainer.innerHTML + `<div class="square yellow"> Buzz </div>`;
-    
+        
     } 
     else {
         squaresContainer.innerHTML = squaresContainer.innerHTML + `<div class="square">${i}</div>`;
     }
+    
+}
+*/
+
+
+
+/*    METODO 'APPEND'   */
+
+let squaresContainer = document.querySelector('.squares-container');
+
+
+for (let i = 1; i <= 100; i++) {
+
+    let elemento = document.createElement('div');
+    elemento.classList.add('square');
+    elemento.innerHTML = i ;
+
+    if (i % 15 == 0) {
+
+        elemento.classList.add('red');
+        elemento.innerHTML = 'FizzBuzz'
+
+    } else if (i % 3 == 0) {
+    
+        elemento.classList.add('green');
+        elemento.innerHTML = 'Fizz'
+    
+    } else if (i % 5 == 0) {
+    
+        elemento.classList.add('yellow');
+        elemento.innerHTML = 'Buzz'
+        
+    }
+
+    squaresContainer.append(elemento)
+
+    console.log(elemento)
+    console.log(i)
     
 }
 
