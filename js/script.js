d3fea0e8808cef3b06bba23551f8e8102c7cc7f1
camillapalombi <camilla.palombi14@gmail.com>
2022-02-23 16:30:10 +0100

    Numeri 1->100 stampati nell'html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+let squaresContainer = document.querySelector('.squares-container');
+
+
 for (let i = 1; i <= 100; i++) {
     
     console.log(i)
+    
+    squaresContainer.innerHTML = squaresContainer.innerHTML + `<div class="square">${i}</div>`;
     
 }
